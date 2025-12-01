b09df523274acb2299e821d3b6c528b29aaddea2
BMV <venerasmith@mail.ru>
2025-12-01 17:28:56 +0300

    fix merge

---
+++ hw09/src/main/java/ru/otus/hw/dto/CommentViewDto.java
@@ -0,0 +1,16 @@
+package ru.otus.hw.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentViewDto {
+    private Long id;
+
+    private Long bookId;
+
+    private String content;
+}
