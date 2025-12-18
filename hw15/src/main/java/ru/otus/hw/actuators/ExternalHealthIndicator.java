49b1cf2c249c6d8889de1644777436e5a3223ef7
BMV <venerasmith@mail.ru>
2025-12-18 23:48:37 +0300

    try catch block for ExternalHealthIndicator

--- hw15/src/main/java/ru/otus/hw/actuators/ExternalHealthIndicator.java
+++ hw15/src/main/java/ru/otus/hw/actuators/ExternalHealthIndicator.java
@@ -1,6 +1,7 @@
 package ru.otus.hw.actuators;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
@@ -10,16 +11,23 @@ import ru.otus.hw.repositories.BookRepository;
 @Component
 @ConditionalOnEnabledHealthIndicator("external")
 @RequiredArgsConstructor
+@Slf4j
 public class ExternalHealthIndicator implements HealthIndicator {
 
     private final BookRepository bookRepository;
 
     @Override
     public Health health() {
-        var bookCount = bookRepository.count();
-        if (bookCount == 0) {
-            return new Health.Builder().down().withDetail("books", bookCount).build();
+        try {
+            var bookCount = bookRepository.count();
+            if (bookCount == 0) {
+                return new Health.Builder().down().withDetail("books", bookCount).build();
+            }
+
+            return new Health.Builder().up().withDetail("books", bookCount).build();
+        } catch (Exception ex) {
+            log.error("Book repository error", ex);
+            return new Health.Builder().down().withDetail("repository error", "see logfile").build();
         }
-        return new Health.Builder().up().withDetail("books", bookCount).build();
     }
 }
